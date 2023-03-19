9cad457e382993086f75ac3249cfe843b12cc9b5
Priya Gupta <70740431+priyagupta03@users.noreply.github.com>
2023-03-19 10:07:00 +0530

    Add files via upload

---
+++ currency-converter-chatbot-backend/app.py
@@ -0,0 +1,22 @@
+# import pip
+# pip.main(['install','flask'])
+#
+#
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['POST'])
+def index():
+    data = request.get_json()
+    source_currency = data['queryResult']['parameters']['unit-currency']['currency']
+    amount = data['queryResult']['parameters']['unit-currency']['amount']
+    target_currency = data['queryResult']['parameters']['currency-name']
+
+    print(source_currency)
+    print(amount)
+    print(target_currency)
+    return "<h1>Hello</h1>"
+
+if __name__ == "__main__":
+    app.run(debug = True)
